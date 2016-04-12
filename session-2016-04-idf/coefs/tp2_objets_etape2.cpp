da310fa95c651f53fc14cb00ec73f869e6b8e303
David Chamont <chamont@in2p3.fr>
2016-04-12 16:19:42 +0200

    etape 2 de l'objet

---
+++ session-2016-04-idf/coefs/tp2_objets_etape2.cpp
@@ -0,0 +1,227 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <cstdlib>
+
+
+//==============================================
+// utilitaires
+//==============================================
+
+void echec( unsigned int code, std::string commentaire )
+ {
+  std::cout<<"[ERREUR "<<code<<" : "<<commentaire<<"]"<<std::endl ;
+  exit(code) ;
+ }
+
+int fois_puissance_de_deux( int nombre, int exposant )
+ {
+  if (exposant>0) { nombre <<= exposant ; }
+  else  { nombre >>= -exposant ; }
+  return nombre ;
+ }
+
+double arrondi( double d, unsigned precision =0 )
+ {
+  double mult {1.} ;
+  while (precision-->0) mult *= 10. ;
+  if (d>0) { return int(d*mult+.5)/mult ; }
+  else { return int(d*mult-.5)/mult ; }
+ }
+
+int entier_max( int nombre_bits )
+ { return (fois_puissance_de_deux(1,nombre_bits)-1) ; }
+
+
+//==============================================
+// calculs
+//==============================================
+
+class Coef
+ {
+  public :
+  
+    void init( int bits )
+     { bits_ = bits ; }
+    
+    int lit_bits()
+     { return bits_ ; }
+    
+    // transformation d'un double en Coef
+    void approxime( double valeur )
+     {
+      numerateur_ = exposant_ = 0 ;
+      double min = (entier_max(bits_)+0.5)/2 ;
+      while (valeur<min)
+       {
+        exposant_ = exposant_ + 1 ;
+        valeur = valeur * 2 ;
+       }
+      numerateur_ = arrondi(valeur) ;
+     }
+    
+    // transformation d'un Coef en double
+    double approximation()
+     { return double(numerateur_)/fois_puissance_de_deux(1,exposant_) ; }
+    
+    int multiplie( int e )
+     { return fois_puissance_de_deux(numerateur_*e,-exposant_) ; }
+    
+
+    std::string texte()
+     { return std::to_string(numerateur_)+"/2^"+std::to_string(exposant_) ; }
+
+  private :
+  
+    int bits_ ;
+    int numerateur_ ;
+    int exposant_ ;
+    
+ } ;
+
+
+
+
+//==============================================
+// tests
+//==============================================
+
+class Testeur
+ {
+
+  public :
+
+    void init( int resolution )
+     { resolution_ = resolution ; }
+    
+    virtual void execute( int bits )
+     {
+      std::cout << "Mais qu'est-ce que je fais là ?" << std::endl ;
+     }
+
+  protected :
+  
+    void erreur( int bits, double exact, double approx )
+     {
+      int erreur = arrondi(resolution_*double(exact-approx)/exact) ;
+      if (erreur<0) { erreur = -erreur ; }
+      std::cout
+        <<std::right<<std::setw(2)<<bits<<" bits : "
+        <<std::left<<exact<<" ~ "<<approx
+        <<" ("<<erreur<<"/"<<resolution_<<")" ;
+     }
+    
+  private :
+
+    int resolution_ ;
+ } ;
+
+
+class TesteurCoef : public Testeur
+ {
+  public :
+  
+    void execute( int bits )
+     {
+      c_.init(bits) ;
+      teste(0.65) ;
+      teste(0.35) ;
+     }
+
+  private :
+  
+    void teste( double valeur )
+     {
+      c_.approxime(valeur) ;
+      double approximation = c_.approximation() ;
+      erreur(c_.lit_bits(),valeur,approximation) ;
+      std::cout<<" ("<<c_.texte()<<")"<<std::endl ;
+     }
+    
+    Coef c_ ;
+
+ } ;
+ 
+class TesteurSomme : public Testeur
+ {
+  public :
+    void execute( int bits )
+     {
+      c1_.init(bits) ;
+      c2_.init(bits) ;
+      teste(0.65,3515,0.35,4832) ;
+     }
+  private :
+    void teste( double c1, int e1, double c2, int e2 )
+     {
+      c1_.approxime(c1) ;
+      c2_.approxime(c2) ;
+      int exact, approx ;
+      exact = arrondi(c1*e1+c2*e2) ;
+      approx = c1_.multiplie(e1) + c2_.multiplie(e2) ;
+      erreur(c1_.lit_bits(),exact,approx) ;
+      std::cout<<std::endl ;
+     }
+    Coef c1_ ;
+    Coef c2_ ;
+ } ;
+
+
+//==============================================
+// fonction principale
+//==============================================
+
+class Boucle
+ {
+  public :
+    void init( int taille )
+     {
+      taille_ = taille ;
+      indice_ = 0 ;
+      testeurs_ = new Testeur * [taille_] ;
+     }
+    void acquiere( Testeur * pt )
+     {
+      if (indice_==taille_)
+       { echec(10,"trop de testeurs") ; }
+      testeurs_[indice_++] = pt ;
+     }
+    void execute( int resolution, int debut, int fin, int inc )
+     {
+      for ( int i=0; i<indice_ ; i++ )
+       {
+        if (testeurs_[i]!=nullptr)
+         {
+          std::cout<<std::endl ;
+          testeurs_[i]->init(resolution) ;
+          for ( int bits =debut ; bits <= fin ; bits = bits + inc )
+           { testeurs_[i]->execute(bits) ; }
+         }
+       }
+     }
+    void finalise()
+     {
+      for ( int i=0; i<indice_ ; i++ )
+       { delete testeurs_[i] ; }
+      delete [] testeurs_ ;
+     }
+    
+  private :
+  
+    int taille_ ;
+    int indice_ ;
+    Testeur * * testeurs_ ;
+    
+ } ;
+ 
+int main()
+ {
+  Boucle boucle ;
+  boucle.init(2) ;
+  boucle.acquiere(new TesteurCoef) ;
+  boucle.acquiere(new TesteurSomme) ;
+  boucle.execute(1000000,4,16,4) ;
+  boucle.finalise() ;
+  std::cout << std::endl ;
+  return 0 ;
+ }
